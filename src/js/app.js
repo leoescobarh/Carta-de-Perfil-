d3487302b07c67c9d215040dc88b36f935139298
leoescobarh <ldescobarherrera@gmail.com>
2022-06-29 00:17:12 +0000

    Finalizacion conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Tu name"}</h1>
+          <h2>${variables.lastname ? variables.lastname : "Apellido"}</h2>
+          <h3>${variables.role ? variables.role : "Rol "}</h3>
+          <h3>${variables.city ? variables.city : "Ciudad"}, ${
+    variables.country ? variables.country : "Pais"
+  }</h3>
+         
+          <ul class = ${variables.socialMediaPosition}>
+           <li ><a href="https://twitter.com/"${
+             variables.twitter
+           }><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/"${
+              variables.github
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/"${
+              variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/"${
+              variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
